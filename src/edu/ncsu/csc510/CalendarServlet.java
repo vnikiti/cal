2e852591269f503b95fef9499394bddd204e4dd4
Andrew Miller <vnikiti@ncsu.edu>
2015-03-24 20:05:57 -0400

    Adding a JSON endpoint.

---
+++ src/edu/ncsu/csc510/CalendarServlet.java
@@ -0,0 +1,40 @@
+package edu.ncsu.csc510;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CalendarServlet
+ */
+@WebServlet(asyncSupported = true, urlPatterns = { "/calendar" })
+public class CalendarServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public CalendarServlet() {
+        // TODO Auto-generated constructor stub
+    	
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    List<String> list = new ArrayList<String>();
+	    list.add("item1");
+	    list.add("item2");
+	    list.add("item3");
+	    String json = new Gson().toJson(list);
+
+	    response.setContentType("application/json");
+	    response.setCharacterEncoding("UTF-8");
+	    response.getWriter().write(json);
+	}
+
+}
