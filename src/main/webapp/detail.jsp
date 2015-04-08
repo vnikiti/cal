38172450a4520489fa376e449d4a94724db173f3
Mitchell <mdnevill@ncsu.edu>
2015-04-07 18:25:30 -0700

    Changed detail JSP

--- src/main/webapp/detail.jsp
+++ src/main/webapp/detail.jsp
@@ -1,8 +1,29 @@
+
+
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <%@taglib prefix="t" tagdir="/WEB-INF/tags" %>
 
 <t:layout>
     <div id="event-detail-container" class="row">
+        <div class="media-left">
+            <a href="#"><img id="calImg" class="media-object" src="" style="height: 128px; height: 128px;" /></a>
+        </div>
+        <div class="media-body">
+            <h2 class="media-heading" id="title">${title}</h2>
+            <h4 id="start">${start}</h4>
+            <h4 id="location">${location}</h4>
+            <p>
+                <a id="addToCalLink" href="#"><img src="" /></a>
+            </p>
+        </div>
+        <hr>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h2 class="panel-title">Details</h2>
+            </div>
+            <div class="panel-body" id="description">${description}
+            </div>
+        </div>
 
     </div>
 </t:layout>
@@ -12,9 +33,12 @@
     $(document).ready(function(){
         var config = {
             container : $("#event-detail-container"),
+            cid: '',
+            eid: '',
+            calImgSrc: '',
         };
 
         eventDetails.init(config);
-
+        // eventDetails.displayDemo();
     });
 </script>
--- src/main/webapp/scripts/EventDetails.js
+++ src/main/webapp/scripts/EventDetails.js
@@ -3,9 +3,20 @@
     eventDetails.initialized = false;
 
     eventDetails.config = {
-        container : $('#container'),
-        eventUrl : '/'
+        container : $('#event-detail-container'),
+        eventDetailUrl : '/eventDetail',
+        cid: '',
+        eid: '',
+        calImgSrc: '',
+        location: $("#location"),
+        description: $("#description"),
+        start: $("#start"),
+        calImg: $("#calImg"),
+        title: $("#title"),
+        addToCalLink: $("#addToCalLink")
     };
+
+    var $location, $calImg, $title, $start, $description;
 
     eventDetails.init = function(config){
 
@@ -14,8 +25,49 @@
         }
 
         eventDetails.initialized = true;
+
+        // Cache some jQuery objects for manipulation
+        $location = eventDetails.config.location;
+        $calImg = eventDetails.config.calImg;
+        $title = eventDetails.config.title;
+        $start = eventDetails.config.start;
+        $description = eventDetails.config.description;
     };
 
+    var getDetails = function(){
+        var request = $.ajax({
+                    method: 'GET',
+                    url: eventDetails.config.eventsUrl,
+                    data: {cid: eventDetails.config.cid,
+                            eid: eventDetails.config.eid}
+                });
+                // Process the request
+                request.done(function(msg){
+                    if(msg == null){
+                        console.log(msg);
+                    } else {
+                        displayEventDetails(msg, eventDetails.calImgSrc)
+                    }
+                });
+
+                request.fail(function(jqXHR, textStatus){
+                    console.log("Request failed: " + textStatus);
+                });
+    };
+
+    var displayEventDetails = function(event, imgSrc){
+        $location.text(event.location);
+        $title.text(event.summary);
+        $description.text(event.description);
+        $start.text(event.start.dateTime);
+        $calImg.attr("src", imgSrc);
+    };
+
+
+    eventDetails.displayDemo = function(){
+        var event = {"created":{"value":1424559263000,"dateOnly":false,"tzShift":0},"creator":{"displayName":"John Blondin","email":"blondin@ncsu.edu"},"description":"Alex Bataller, Department of Physics, UCLA\nSono(less)luminescence","end":{"dateTime":{"value":1425051000000,"dateOnly":false,"tzShift":-300}},"etag":"\"2849118527960000\"","htmlLink":"https://www.google.com/calendar/event?eid\u003daHZjcXBrNjlzdnU5NnJyZGJ2YnBsZmpmZmMgbmNzdS5lZHVfdmQ0Z3Y4dGVyNGtscjlzYTZlZm01dm1zcTBAZw","iCalUID":"hvcqpk69svu96rrdbvbplfjffc@google.com","id":"hvcqpk69svu96rrdbvbplfjffc","kind":"calendar#event","location":"RD 106","organizer":{"displayName":"NCSU Physics Events","email":"ncsu.edu_vd4gv8ter4klr9sa6efm5vmsq0@group.calendar.google.com","self":true},"reminders":{"useDefault":true},"sequence":0,"start":{"dateTime":{"value":1425047400000,"dateOnly":false,"tzShift":-300}},"status":"confirmed","summary":"CMB seminar","updated":{"value":1424559263980,"dateOnly":false,"tzShift":0}};
+        displayEventDetails(event, "http://www.nodc.noaa.gov/media/images/seminars/calendar_plus_en.gif");
+    };
 
 }(window.eventDetails = window.eventDetails || {}, window, document, jQuery));
 
